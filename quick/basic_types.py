8e45649a8f2f31f88f04f682fd28ce42a9e7feb2
Alex Myasoedov <msoedov@gmail.com>
2015-09-02 18:11:30 +0300

    Reflect generator for primitive types

--- quick/basic_types.py
+++ quick/basic_types.py
@@ -15,4 +15,18 @@ def random_for(primitive_type):
         return ''
     elif primitive_type == bytes:
         size = random.randint(0, generation_width)
-        return bytes(random.randint(size))
+        return bytes([random.randint(0, 255) for _ in range(size)])
+
+
+def default(type, lo=-max_num, hi=max_num, complex_size=generation_width, min_complexity=0):
+    if type == float:
+        return random.uniform(lo, hi)
+    elif type == int:
+        return random.randint(lo, hi)
+    elif type == str:
+        return ''
+    elif type == bytes:
+        size = random.randint(min_complexity, complex_size)
+        lo = 0 if lo < 0 else lo
+        hi = 255 if hi > 255 else hi
+        return bytes([random.randint(lo, hi) for _ in range(size)])
