d2a2b74729798197929f14d33758793b9e8166ab
Alex Myasoedov <msoedov@gmail.com>
2015-09-30 18:45:04 +0300

    Optional experiment skip on first failure

--- quick/features.py
+++ quick/features.py
@@ -1,5 +1,6 @@
 import sys
 import unittest
+import functools
 from collections import namedtuple
 from .core import generate
 import concurrent.futures
@@ -31,20 +32,49 @@ class QuickCheck(object):
 
     forall = __call__
 
-    def as_testcase(self, prototype=unittest.TestCase):
+    def as_testcase(self, prototype=unittest.TestCase, skip_on_failure=True):
+        """
+        :param prototype: class of test case
+        :param skip_on_failure: boolean flag to skip all test group on first failure
+        :return: test case class
+        """
 
         class TestProperties(prototype):
             """
             Automatically generated tests case based on quick check properties
             """
 
+        def skip_if():
+
+            skip = False
+
+            def wrap(fn):
+
+                @functools.wraps(fn)
+                def inner(*args, **kwargs):
+                    nonlocal skip
+                    if skip and skip_on_failure:
+                        raise unittest.SkipTest('Failed experiment')
+                    try:
+                        return fn(*args, **kwargs)
+                    except Exception as e:
+                        skip = True
+                        raise e
+
+                return inner
+
+            return wrap
+
         settings = self.settings
         for experiment in self.experiments.values():
             if experiment.config is not default:
                 settings = experiment.config
             max_count = settings['max_count']
+
+            skip_group = skip_if()
             for x in range(max_count):
 
+                @skip_group
                 def test_experiment(t):
                     test_case, input = generate(experiment.fn)
                     ok = test_case(**input)
