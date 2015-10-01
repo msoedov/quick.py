edd8db659ea87840fcc3f19c660ee02dadccb098
Alex Myasoedov <msoedov@gmail.com>
2015-10-01 15:56:57 +0300

    Shrinking for unknown struct

--- quick/shrink.py
+++ quick/shrink.py
@@ -7,12 +7,20 @@ class GiveUp(Exception):
 
 def shrink(validator, input):
     """
+    # basic example
     >>> validator = lambda x: 1 == x[-1]
     >>> shrink(validator, {'x': [1, 2, 3]})
     (True, {'x': [1]})
+
+    # It should return input as is for unknown structures
+    >>> validator = lambda x: 1 == x[-1]
+    >>> shrink(validator, {'x': None})
+    (False, {'x': None})
     """
     for var, value in input.items():
-        strategy = strategies_per_type[type(value)]
+        strategy = strategies_per_type.get(type(value))
+        if not strategy:
+            continue
         simplified = strategy(value)
         while not validator(simplified):
             try:
